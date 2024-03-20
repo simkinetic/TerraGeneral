79e085d9175ccfaf4f670940bb98a85067c9ac1b
rrhiemstar <rrhiemstar@gmail.com>
2024-03-20 12:29:27 +0100

    Initialized new registry.

---
+++ Registry.lua
@@ -0,0 +1,9 @@
+local Registry = {
+    name = "TerraGeneral",
+    uuid = "9fb6749b-ce79-47f1-a769-01fb5a196baa",
+    url  = "git@github.com:simkinetic/TerraGeneral.git",
+    description = "Cosm local package registry",
+    packages = {
+    },
+}
+return Registry
